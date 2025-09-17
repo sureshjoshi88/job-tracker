456bf8159aa31337fa1ef9d0163218b2c9bff402
sureshjoshi88 <sj3795789@gmail.com>
2025-09-17 16:14:24 +0530

    add new state

--- client/src/components/AllJobs.jsx
+++ client/src/components/AllJobs.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react'
 
 const AllJobs = () => {
   const [data,setData] =useState([])
+  const [company,setCompany] = useState('')
   useEffect(()=>{
     fetch('http://localhost:3000/job/alljob')
     .then((data)=>data.json())
@@ -17,18 +18,18 @@ const AllJobs = () => {
   
   return (
     <div>
-      <div className='flex justify-center'>
-        <form action="">
-          <input  className='border-b rounded p-2 outline-0' type="text" placeholder='Enter your Company name' required /><br/>
-          <input  className='border-b rounded p-2 outline-0' type="text" placeholder='Enter your Position' required /><br/>
-          <input  className='border-b rounded p-2 outline-0' type="text" placeholder='Enter your Location' required /><br/>
-          <select name="" id="" className='border-b rounded p-2 outline-0'>
+      <div className='flex justify-center mt-3'>
+        <form action="" className='shadow-2xl p-4 rounded space-y-4 w-100'>
+          <input  className='border-b rounded p-2 outline-0 w-full' type="text" placeholder='Enter your Company name' required /><br/>
+          <input  className='border-b rounded p-2 outline-0 w-full' type="text" placeholder='Enter your Position' required /><br/>
+          <input  className='border-b rounded p-2 outline-0 w-full' type="text" placeholder='Enter your Location' required /><br/>
+          <select name="" id="" className='border-b rounded p-2 outline-0 w-full'>
             <option value="applied">Applied</option>
             <option value="pending">Pending</option>
             <option value="reject">Reject</option>
           </select><br/>
-          <input  className='border-b rounded p-2 outline-0' type="text" placeholder='Enter your description' required /><br/>
-          <button className='mt-3 border rounded p-2 '>Add Job</button>
+          <input  className='border-b rounded p-2 outline-0 w-full' type="text" placeholder='Enter your description' required /><br/>
+          <button className='mt-3 border rounded p-2 w-full'>Add Job</button>
         </form>
       </div>
       <div className='grid gap-4 grid-cols-3 mt-5'>
