cc2860578975f0184d357f069182d0bd47043c60
sureshjoshi88 <sj3795789@gmail.com>
2025-09-17 11:06:54 +0530

    updates

--- server/src/index.js
+++ server/src/index.js
@@ -7,10 +7,9 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
-
 connectDb();
-app.use('/job',routers)
+app.use('/job', routers)
 const port = process.env.PORT
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`the server in running http://localhost:${port}`);
 })
