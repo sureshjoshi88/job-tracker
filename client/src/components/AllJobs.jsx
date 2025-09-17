a989ab7ff2a3fd274e8b87c8e777303a15809e23
sureshjoshi88 <sj3795789@gmail.com>
2025-09-17 16:03:02 +0530

    add new style

--- client/src/components/AllJobs.jsx
+++ client/src/components/AllJobs.jsx
@@ -17,12 +17,21 @@ const AllJobs = () => {
   
   return (
     <div>
-      <div>
+      <div className='flex justify-center'>
         <form action="">
-          <input  className='border-b rounded p-2 outline-0' type="text" placeholder='Enter your Company name' required />
+          <input  className='border-b rounded p-2 outline-0' type="text" placeholder='Enter your Company name' required /><br/>
+          <input  className='border-b rounded p-2 outline-0' type="text" placeholder='Enter your Position' required /><br/>
+          <input  className='border-b rounded p-2 outline-0' type="text" placeholder='Enter your Location' required /><br/>
+          <select name="" id="" className='border-b rounded p-2 outline-0'>
+            <option value="applied">Applied</option>
+            <option value="pending">Pending</option>
+            <option value="reject">Reject</option>
+          </select><br/>
+          <input  className='border-b rounded p-2 outline-0' type="text" placeholder='Enter your description' required /><br/>
+          <button className='mt-3 border rounded p-2 '>Add Job</button>
         </form>
       </div>
-      <div className='grid gap-4 grid-cols-3 '>
+      <div className='grid gap-4 grid-cols-3 mt-5'>
         {
           data.map((item,index)=>{
            return <div className='border rounded p-3'>
